03260255135bcad6c331f9c838d1803c93647ee1
Shivargha Bandopadhyay <sbjeetmeet11@gmail.com>
2025-06-30 12:23:35 +0530

    chatbot with tools updated

--- chatbot_experiments/basic_chatbot.py
+++ chatbot_experiments/basic_chatbot.py
@@ -18,6 +18,12 @@ def chatNode(state:BasicChatState):
         "messages": [llm_model.invoke(state["messages"])] 
     }
 
+"""state["messages"] is a list of BaseMessages (e.g., HumanMessage, SystemMessage, etc.)
+Invoke the llm_model on those messages using llm_model.invoke(...)
+The result ~ AIMessage is wrapped in a new "messages" list and added to the new state"""
+
+
+
 ##Creating the entire graph##
 graph = StateGraph(BasicChatState)
 ## add the single chatnode ##
@@ -33,6 +39,8 @@ image.save("chatbot_experiments/basic_chatbot.png")
 ###looping the chatbot###
 
 ## graph is restarting at every while loop ##
+
+
 while True:
     user_input = input("User: ")
     if (user_input in ["exit","end"]):
@@ -40,7 +48,17 @@ while True:
     else:
         result = app.invoke(
 
-            {"messages":[HumanMessage(content=user_input)]}
+            {"messages":[HumanMessage(content=user_input)]} ##start with the Human message
         )
 
         print(result)
+
+'''
+The first node in your LangGraph gets this state:
+state = {
+    "messages": [HumanMessage(...)]
+}
+Your node (e.g., chatNode) does:
+llm_response = llm_model.invoke(state["messages"])
+
+'''
