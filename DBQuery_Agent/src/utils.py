1c7c1eae9bbf261cc8d25450477122218a37bbd6
shivargha98 <sbjeetmeet11@gmail.com>
2025-11-07 11:05:16 +0530

    test

--- DBQuery_Agent/src/utils.py
+++ DBQuery_Agent/src/utils.py
@@ -14,7 +14,7 @@ db_path = 'Chinook_Sqlite.sqlite'
 schema_knowledge = describe_schema(db_path)
 
 llm_model = ChatGoogleGenerativeAI(model = 'gemini-2.0-flash',max_retries=2)
-llm_qwen_coder = ChatOllama(model="qwen2.5-coder:3b")
+llm_qwen_coder = ChatOllama(model="qwen2.5:3b")
 #llm_judge = ChatGroq(model = 'llama-3.1-8b-instant')
 llm_judge = ChatGoogleGenerativeAI(model = 'gemini-2.0-flash',max_retries=2)
 
